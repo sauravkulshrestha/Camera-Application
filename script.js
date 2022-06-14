43e7bf46ea1306135b75d2b11e82898e249cbf2c
saurav <sauravkul0@gmail.com>
2022-06-14 20:34:11 +0530

    timer application implemeted

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let shouldRecord = false;
 let chunks = [];
 let constraints = {
     video : true,
-    audio : false 
+    audio : false
 }
    navigator.mediaDevices.getUserMedia(constraints)    
     .then((stream) => {
@@ -18,27 +18,40 @@ let constraints = {
 
          recorder.addEventListener("start" , ()=>{
              chunks = [];
+             console.log('rec started');
          });
             
 
          recorder.addEventListener("dataavaliable" ,(e)=>{
              chunks.push(e.data);
+             console.log('recording stored in chunks');
          });
 
          recorder.addEventListener("stop" , ()=>{
             //  conver video 
-            //  let blob = new Blob(chunks , {type : 'video/mp4'});
+            console.log('recording stoped');
+             let blob = new Blob(chunks , {type : 'video/mp4'});
 
             // download video on desktop
+
+            let videoURL = URL.createObjectURL(blob);
+            console.log(videoURL);
+
+            let a = document.createElement('a');
+            a.href = videoURL;
+            a.download = 'myVideo.mp4';
+            a.click();
+
             // store in data base;
 
          })
 
     });
 
-    
+       
     captureBtnCont.addEventListener("click", () => {
         console.log("camera icon clicked");
+        captureBtn.classList.add("scale-capture");
         let canvas = document.createElement("canvas");
         let tool = canvas.getContext("2d");
         canvas.width = video.videoWidth;
@@ -51,10 +64,16 @@ let constraints = {
         tool.fillRect(0, 0, canvas.width, canvas.height);
         
         let imageURL = canvas.toDataURL();
-        let img = document.createElement("img");
-        img.src = imageURL; //
-        document.body.append(img);
+        // let img = document.createElement("img");
+        // img.src = imageURL; 
+        // document.body.append(img);
         
+
+
+        setTimeout(()=>{
+            captureBtn.classList.remove("scale-capture");
+        }, 510);
+
     });
 
     recordBtnCont.addEventListener("click" , ()=>{
@@ -63,12 +82,64 @@ let constraints = {
             // start recording
             recorder.start();
             // start timer
+
+
             startTimer();   
         }else{
             // stop recording
             recorder.stop();
+            
             // stop timer
             stopTimer();   
+
         }
+    });
+
+    // timer start n stop
+
+    let timer = document.querySelector(".timer");
+    let counter = 0;
+    let timerID;
+    function startTimer() {
+        timer.style.display = 'block';
+        function displayTimer(){
+            let totalSeconds = counter;
+
+            let hours = Number.parseInt(totalSeconds/3600);
+            totalSeconds =   totalSeconds % 3600;
+
+            let minute = Number.parseInt(totalSeconds/3600);
+            totalSeconds =   totalSeconds % 60;
+             
+            let seconds = totalSeconds;
+
+            hours = (hours < 10) ? `0 ${hours}`:hours;
+            minute = (minute < 10) ? `0 ${minute}`:minute;
+            seconds = (seconds < 10) ? `0 ${seconds}`:seconds;
+            timer.innerText = `${hours}:${minute}:${seconds}`;
+            counter++;
+        }
+
+        timerID = setInterval(displayTimer , 1000);
+        counter = 0;
+
+
+    }
+         function stopTimer() {
+             clearInterval(timerID);
+             timer.innerText = "00:00:00";
+              timer.style.display = 'none';
+         }
+
+let filterLayer = document.querySelector('.filter-layer');
+let allFilters = document.querySelector('.filter');
+
+
+
+allFilters.forEach((filterElem) => {
+    filterElem.addEventListener('click', () => {
+        transparentColor = getComputedStyle(filterElem).getPropertyValue('background-color');
+        filterLayer.style.backgroundColor = transparentColor;
     })
-           
+})
+
